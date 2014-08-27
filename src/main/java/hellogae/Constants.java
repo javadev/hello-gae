3724e862daf8df40fda25cd00a07285888bdd9c7
javadev <javadev75@gmail.com>
2014-08-27 12:09:17 +0300

    Initial commit

---
+++ src/main/java/hellogae/Constants.java
@@ -0,0 +1,13 @@
+package hellogae;
+
+/**
+ * Contains the client IDs and scopes for allowed clients consuming the helloworld API.
+ */
+public class Constants {
+  public static final String WEB_CLIENT_ID = "210467524142-c33omvf4so1cutbm7of25sanh1bi50v4.apps.googleusercontent.com";
+  public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
+  public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
+  public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
+
+  public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
+}
