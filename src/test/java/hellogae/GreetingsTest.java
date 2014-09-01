f51aae74b7d7ef036ed4b8d9dbdecdde3e8aaab9
javadev <javadev75@gmail.com>
2014-09-01 14:42:12 +0300

    Add more unit tests

--- src/test/java/hellogae/GreetingsTest.java
+++ src/test/java/hellogae/GreetingsTest.java
@@ -6,6 +6,8 @@
 
 package hellogae;
 
+import com.google.api.server.spi.response.NotFoundException;
+import com.google.appengine.api.users.User;
 import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
@@ -24,6 +26,12 @@ public class GreetingsTest {
        assertEquals("goodbye world!", helloGreeting1.getMessage());
     }
     
+    @Test(expected = NotFoundException.class)
+    public void getGreetingException() throws Exception {
+       Greetings greetings = new Greetings();
+       greetings.getGreeting(2);
+    }
+    
     @Test
     public void listGreeting() {
        Greetings greetings = new Greetings();
@@ -39,6 +47,14 @@ public class GreetingsTest {
        Greetings greetings = new Greetings();
        HelloGreeting resultHelloGreeting = greetings.insertGreeting(2, helloGreeting);
        assertEquals("TestTest", resultHelloGreeting.getMessage());
+    }
+    
+    @Test
+    public void authedGreeting() {
+        Greetings greetings = new Greetings();
+        User user = new User("email@test.com", "test.com");
+        HelloGreeting helloGreeting = greetings.authedGreeting(user);
+        assertEquals("hello email@test.com", helloGreeting.getMessage());
     }
 
 }
